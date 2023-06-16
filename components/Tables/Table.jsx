d519f299439bc5d0c4cd2b554f10eecf7a64323a
jeff0518 <cycivs07@icloud.com>
2023-06-16 23:37:11 +0800

    feat: data.json

--- components/Tables/Table.jsx
+++ components/Tables/Table.jsx
@@ -1,5 +1,43 @@
-import style from './Table.module.css'
-import {BiEdit, BiTrashAlt} from 'react-icons/bi'
+import style from "./Table.module.css";
+import { BiEdit, BiTrashAlt } from "react-icons/bi";
+import data from "../../database/data.json";
+
+function Tr({ id, name, avatar, email, salary, date, status }) {
+  return (
+    <tr className={style.tbody_tr}>
+      <td className={style.tbody_td}>
+        <div className={style.td_img}>
+          <img src={avatar || "#"} alt="" />
+          <span className={style.td_span}>{name || Unknown}</span>
+        </div>
+      </td>
+      <td className={style.tbody_td}>
+        <span>{email || Unknown}</span>
+      </td>
+      <td className={style.tbody_td}>
+        <span>{salary || Unknown}</span>
+      </td>
+      <td className={style.tbody_td}>
+        <span>{date || Unknown}</span>
+      </td>
+      <td className={style.tbody_td}>
+        <button className={style.td_but}>
+          <span className={style.td_but_span}>{status || Unknown}</span>
+        </button>
+      </td>
+      <td className={style.tbody_td}>
+        <div className={style.tbody_but}>
+          <button className={style.td_but}>
+            <BiEdit size={25} color={"rgb(24,197,94)"} />
+          </button>
+          <button className={style.td_but}>
+            <BiTrashAlt size={25} color={"rgb(244,63,94)"} />
+          </button>
+        </div>
+      </td>
+    </tr>
+  );
+}
 
 function Table() {
   return (
@@ -27,38 +65,9 @@ function Table() {
         </tr>
       </thead>
       <tbody className={style.tbody}>
-        <tr className={style.tbody_tr}>
-          <td className={style.tbody_td}>
-            <div className={style.td_img}>
-              <img src="#" alt="" />
-              <span className={style.td_span}>Daily Tuition</span>
-            </div>
-          </td>
-          <td className={style.tbody_td}>
-            <span>dailytuition@gmail.com</span>
-          </td>
-          <td className={style.tbody_td}>
-            <span>$25000</span>
-          </td>
-          <td className={style.tbody_td}>
-            <span>13-06-2023</span>
-          </td>
-          <td className={style.tbody_td}>
-            <button className={style.td_but}>
-              <span className={style.td_but_span}>Active</span>
-            </button>
-          </td>
-          <td className={style.tbody_td}>
-            <div className={style.tbody_but}>
-              <button className={style.td_but}>
-                <BiEdit size={25} color={"rgb(24,197,94)"} />
-              </button>
-              <button className={style.td_but}>
-                <BiTrashAlt size={25} color={"rgb(244,63,94)"} />
-              </button>
-            </div>
-          </td>
-        </tr>
+        {data.map((obj, i) => (
+          <Tr {...obj} key={i} />
+        ))}
       </tbody>
     </table>
   );
