6bf91e704aa0a503df9029495268ecf8c5461274
jeff0518 <cycivs07@icloud.com>
2023-06-19 01:00:07 +0800

    feat: Post

--- database/controller.jsx
+++ database/controller.jsx
@@ -1,12 +1,24 @@
-
-import Users from "../model/user"
+import Users from "../model/user";
 export async function getUsers(req, res) {
   try {
-    const users = await Users.find({})
+    const users = await Users.find({});
 
-    if(!users) return res.status(404).json({error:"Data not Fount"})
-    res.status(200).json(users)
-  } catch(error) {
-    res.status(404).json({error: "Error While Fetching Data"})
+    if (!users) return res.status(404).json({ error: "Data not Fount" });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(404).json({ error: "Error While Fetching Data" });
+  }
+}
+
+export async function postUser(req, res) {
+  try {
+    const formData = req.body;
+    if (!formData)
+      return res.status(404).json({ error: "Form Data Not Provided...!" });
+    Users.create(formData).then((err, data) => {
+      res.status(200).json(data);
+    });
+  } catch (error) {
+    return res.status(404).json({ error });
   }
 }
--- pages/api/users/index.jsx
+++ pages/api/users/index.jsx
@@ -1,28 +1,28 @@
 import connectMongo from "../../../database/conn";
-import { getUsers } from "../../../database/controller";
+import { getUsers, postUser } from "../../../database/controller";
 
-function handler(req, res) {
-  connectMongo().catch(() => res.status(405).json({error: "Error in the Connection"}));
+export default async function handler(req, res) {
+  connectMongo().catch(() =>
+    res.status(405).json({ error: "Error in the Connection" })
+  );
 
-  const {method} = req
+  const { method } = req;
   switch (method) {
     case "GET":
       getUsers(req, res);
-      break
+      break;
     case "POST":
-      res.status(200).json({ method, name: "POST Request" });
-      break
+      postUser(req, res);
+      break;
     case "PUT":
       res.status(200).json({ method, name: "PUT Request" });
-      break
+      break;
     case "DELETE":
       res.status(200).json({ method, name: "DELETE Request" });
-      break
+      break;
     default:
-      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE'])
-      res.status(405).end(`Method ${method} Not Allow`)
+      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`Method ${method} Not Allow`);
   }
-  res.status(200).json({ name: "John Doe" });
 }
 
-export default handler;
